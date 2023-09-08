60c661484837062803c252426f318d0b82c94213
barsu5489 <ebarsulai@gmail.com>
2023-09-08 18:03:24 +0300

    done

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const miliSec = document.querySelector('.milli-sec')
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 const dayNumber = new Date().getDay()
 const now = new Date
-currentTime.textContent = `Day: ${days[dayNumber]}`
-miliSec.textContent = `Time: ${Date.UTC(now.getUTCMilliseconds())}`
+currentTime.textContent += `${days[dayNumber]}`
+miliSec.textContent += `${Date.UTC(now.getUTCMilliseconds())} seconds`
